442ad56a25869563271c11f66e403d8823a61867
Edd Baldry <edd.baldry@gmail.com>
2023-04-13 13:16:25 +0200

    Reverse chronology and adds checkbox to select only those responses with comments

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,6 +15,7 @@ const IndexPage = () => {
   const [data, setData] = useState<Data | null>(null);
   const [isLoading, setIsLoading] = useState(true);
   const [selected, setSelected] = useState<keyof Data>('thumb_up');
+  const [showOnlyWithComments, setShowOnlyWithComments] = useState(false);
 
   useEffect(() => {
     async function fetchData() {
@@ -36,6 +37,10 @@ const IndexPage = () => {
     setSelected(selection);
   };
 
+  const handleToggleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setShowOnlyWithComments(event.target.checked);
+  };
+
   return (
     <div className="mx-auto max-w-2xl px-4">
       {isLoading ? (
@@ -55,38 +60,59 @@ const IndexPage = () => {
                 <p>{data?.thumb_down.length}</p></div>
             </div>
             <button
-              className={`font-bold py-2 px-4 rounded mr-2 border-2 border-black ${selected === 'thumb_up'
-                ? 'bg-white text-black'
-                : 'bg-black text-white'
+              className={`font-bold py-2 px-4 mr-2 border-b-2 border-black ${selected === 'thumb_up'
+                ? 'bg-black text-white'
+                : 'bg-white text-black'
                 }`}
               onClick={() => switchSelection('thumb_up')}
             >
               Helpful responses
             </button>
             <button
-              className={`font-bold py-2 px-4 rounded border-2 border-black ${selected === 'thumb_down'
-                ? 'bg-white text-black'
-                : 'bg-black text-white'
+              className={`font-bold py-2 px-4 border-b-2 border-black ${selected === 'thumb_down'
+                ? 'bg-black text-white'
+                : 'bg-white text-black'
                 }`}
               onClick={() => switchSelection('thumb_down')}
             >
               Unhelpful responses
             </button>
+            <div className='border-b-4 border-black z-10 relative -mt-1'></div>
           </div>
           <div className="prose">
-            {data && data[selected].map((item, index) => (
-              <div key={index}>
-
-                <p>
-                  <strong>Prompt:</strong> {item.question}
-                  <br />
-                  <strong>Response:</strong> {item.answer}
-                  <br />
-                  <small><strong>Comment:</strong> {item.comment}</small>
-                </p>
-
-              </div>
-            ))}
+            {selected === 'thumb_up' ?
+              <h2>Helpful responses</h2> :
+              <h2>Unhelpful responses</h2>}
+            <div className="flex items-center mt-4">
+              <label htmlFor="toggle" className="mr-2">
+                Show only with comments
+              </label>
+              <input
+                type="checkbox"
+                id="toggle"
+                className="w-5 h-5 rounded border-gray-300 focus:border-gray-100 focus:ring-2 checked:bg-black checkbox"
+                checked={showOnlyWithComments}
+                onChange={handleToggleChange}
+              />
+            </div>
+            {data &&
+              data[selected]
+                .slice() // Create a shallow copy of the array
+                .reverse() // Reverse the order of the copied array
+                .filter((item) => !showOnlyWithComments || (showOnlyWithComments && item.comment))
+                .map((item, index) => (
+                  <div key={index}>
+                    <p>
+                      <strong>Prompt:</strong> {item.question}
+                      <br />
+                      <strong>Response:</strong> {item.answer}
+                      <br />
+                      <small>
+                        <strong>Comment:</strong> {item.comment}
+                      </small>
+                    </p>
+                  </div>
+                ))}
           </div></div>
       )}
     </div>
